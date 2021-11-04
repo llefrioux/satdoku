7d56d9d6dba58863ec2934c699668185eb3f578e
Ludovic Le Frioux <ludovic.le-frioux@inria.fr>
2021-11-04 14:13:55 +0100

    Rename dimacs util functions.

--- sdk2cnf.py
+++ sdk2cnf.py
@@ -40,19 +40,19 @@ args = parser.parse_args()
 ##### Functions #####
 
 # Return a unique value for variables based on the input triple (i, j, k)
-def var_encoding(i, j, k):
+def encode_dimacs_var(i, j, k):
    return (i * args.N + j) * args.N + k
 
-## Print header
-def print_header(n_vars, n_cls):
+# Print dimacs header
+def print_dimacs_header(n_vars, n_cls):
    print(f"p cnf {n_vars} {n_cls}")
 
-## Print comment
-def print_comment(comment):
+# Print dimacs comment
+def print_dimacs_comment(comment):
    print(f"c {comment}")
 
 # Print a clause given as a list of literals
-def print_cls(cls):
+def print_dimacs_cls(cls):
    print(*cls + [0])
 
 
@@ -74,7 +74,6 @@ if args.N == 16:
 else:
    for val in range(1, args.N + 1):
       VALUES[str(val)] = val
-
 
 # Parsing the input file containing the Sudoku grid
 fd    = open(args.sudoku_file)
@@ -103,40 +102,40 @@ n_cls += args.N ** 2 * args.N * (args.N - 1) // 2
 n_cls += args.N ** 2 * args.N * (args.N - 1) // 2
 n_cls += n_assumptions
 
-print_header(n_vars, n_cls)
+print_dimacs_header(n_vars, n_cls)
 
 # Generate clauses forcing at least one digit per square
-print_comment("Forcing at least one digit per square.")
+print_dimacs_comment("Forcing at least one digit per square.")
 for i in range(args.N):
    for j in range(args.N):
       cls = []
       for k in range(1, args.N + 1):
-         cls += [var_encoding(i, j, k)]
-      print_cls(cls)
+         cls += [encode_dimacs_var(i, j, k)]
+      print_dimacs_cls(cls)
 
 
 # Generate clauses forcing a digit not to be twice on a line
-print_comment("Forcing a digit not to be twice on a line.")
+print_dimacs_comment("Forcing a digit not to be twice on a line.")
 for i in range(args.N):
    for k in range(1, args.N + 1):
       for j1 in range(args.N):
          for j2 in range(j1 + 1, args.N):
-            cls = [-var_encoding(i, j1, k), -var_encoding(i, j2, k)]
-            print_cls(cls)
+            cls = [-encode_dimacs_var(i, j1, k), -encode_dimacs_var(i, j2, k)]
+            print_dimacs_cls(cls)
 
 
 # Generate clauses forcing a digit not to be twice on a column
-print_comment("Forcing a digit not to be twice on a column.")
+print_dimacs_comment("Forcing a digit not to be twice on a column.")
 for j in range(args.N):
    for k in range(1, args.N + 1):
       for i1 in range(args.N):
          for i2 in range(i1 + 1, args.N):
-            cls = [-var_encoding(i1, j, k), -var_encoding(i2, j, k)]
-            print_cls(cls)
+            cls = [-encode_dimacs_var(i1, j, k), -encode_dimacs_var(i2, j, k)]
+            print_dimacs_cls(cls)
 
 
 # Generate clauses forcing a digit not to be twice in a region
-print_comment("Forcing a digit not to be twice on a region.")
+print_dimacs_comment("Forcing a digit not to be twice on a region.")
 region_size = int(math.sqrt(args.N))
 for I in range(region_size):
    for J in range(region_size):
@@ -145,15 +144,15 @@ for I in range(region_size):
             for j in range(J * region_size, (J + 1) * region_size):
                for j1 in range(j + 1, (J + 1) * region_size):
                   for i1 in range(I * region_size, (I + 1) * region_size):
-                     cls = [-var_encoding(i, j, k), -var_encoding(i1, j1, k)]
-                     print_cls(cls)
+                     cls = [-encode_dimacs_var(i, j, k), -encode_dimacs_var(i1, j1, k)]
+                     print_dimacs_cls(cls)
                for i1 in range(i + 1, (I + 1) * region_size):
-                  cls = [-var_encoding(i, j, k), -var_encoding(i1, j, k)]
-                  print_cls(cls)
+                  cls = [-encode_dimacs_var(i, j, k), -encode_dimacs_var(i1, j, k)]
+                  print_dimacs_cls(cls)
 
 
 # Generate clauses forcing the respect of given assumptions
-print_comment("Forcing the respect of given assumptions")
+print_dimacs_comment("Forcing the respect of given assumptions")
 for i in range(args.N):
    for j in range(args.N):
       k = grid[i][j]
@@ -161,5 +160,5 @@ for i in range(args.N):
       if k == VALUES["."]:
          continue
 
-      unit_cls = [var_encoding(i, j, k)]
-      print_cls(unit_cls)
+      unit_cls = [encode_dimacs_var(i, j, k)]
+      print_dimacs_cls(unit_cls)
